1db303ff6c84aaca51689cb81c1944a4a54c79e1
CCliftS <90357585+CCliftS@users.noreply.github.com>
2023-10-05 23:52:18 -0300

    UPDATE: SignInScreen

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, StyleSheet, TextInput, Image, Text, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 
-const SignInScreen = ({ navigation }: any) => {
-
+const  SignInScreen = ({navigation}: any) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
-  const [lastName, setLastname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleRegister = async (email:string,password:string,name:string,lastname:string) => {
+
+  const handleRegister = async (
+    email: string,
+    password: string,
+    name: string,
+    lastName: string
+  ) => {
     setError(false);
 
     try {
@@ -21,52 +26,55 @@ const SignInScreen = ({ navigation }: any) => {
         name,
         lastName,
       });
-      navigation.navigate("Login");
-    }catch (e: any){
+      navigation.navigate('Login');
+    } catch (e: any) {
       setError(true);
-      setErrorMessage("ERROR AL REGISTRARSE");
+      setErrorMessage(e?.response?.data?.message);
+      console.error({ error: e?.response?.data?.message });
     }
   };
-
   return (
     <View style={styles.container}>
-      <Image 
-            source={require('./../public/gira.png')}
-            style={styles.logo}
-          />
+      <Image
+        source={require('./../public/gira.png')}
+        style={styles.logo}
+      />
       <View style={styles.container2}>
         <Text style={styles.title}>Registro</Text>
         <TextInput
           placeholder="Correo Electrónico"
           value={email}
-          onChangeText={setEmail}
+          onChangeText={(text: string) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Contraseña"
           value={password}
-          onChangeText={setPassword}
+          onChangeText={(text: string) => setPassword(text)}
           secureTextEntry
           style={styles.input}
         />
         <TextInput
           placeholder="Nombre"
           value={name}
-          onChangeText={setName}
+          onChangeText={(text: string) => setName(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Apellido"
-          value={lastName}
-          onChangeText={setLastname}
+          value={lastname}
+          onChangeText={(text: string) => setLastname(text)}
           style={styles.input}
         />
 
-        <TouchableOpacity onPress={() => handleRegister(email,password,name,lastName)} style={[styles.button]}>
+        <TouchableOpacity
+          style={[styles.button]}
+          onPress={() => handleRegister(email, password, name, lastname)}
+        >
           <Text style={styles.buttonText}>Registrarse</Text>
         </TouchableOpacity>
-        <Text onPress={()=> navigation.navigate("Login")} style={styles.link}>
-              Ya tienes una cuenta? Inicia Sesión.
+        <Text onPress={() => navigation.navigate("Login")} style={styles.link}>
+          Ya tienes una cuenta? Inicia Sesión.
         </Text>
       </View>
     </View>
@@ -78,12 +86,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'#0747a6',
+    backgroundColor: '#0747a6',
   },
-  container2:{
+  container2: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'white',
+    backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingBottom: 10,
     borderRadius: 5,
@@ -91,7 +99,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     padding: 16,
-    color:'#85828a',
+    color: '#85828a',
   },
   input: {
     width: 240,
@@ -107,16 +115,16 @@ const styles = StyleSheet.create({
     color: 'blue',
     marginBottom: 16,
   },
-  button:{
+  button: {
     backgroundColor: '#0052cc',
     borderRadius: 15,
     alignItems: 'center',
     height: 40,
     width: 150,
   },
-  buttonText:{
-    color:'white',
-    textAlign:'center',
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
     marginTop: 5,
     fontSize: 20,
   },
