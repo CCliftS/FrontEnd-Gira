5547d993952ae34510567a67050a4d64a1a1e96b
Joshir Contreras <joshir.contreras@gmail.com>
2023-12-15 23:02:05 -0300

    FIX : elimiando codigo basura

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,3 +1,9 @@
 declare module 'react-native-dotenv' {
-    export const ENDPOINT_MS_USER: 'http://10.0.2.2:3000';
-  }
+  export const ENDPOINT_MS_USER: string;
+  export const ENDPOINT_MS_TEMAMS: string;
+  export const ENDPOINT_MS_TASK: string;
+};
+
+ENDPOINT_MS_USER = http: /10.0.2.2:3000
+ENDPOINT_MS_PROJECTS = http: /10.0.2.2:3001
+ENDPOINT_MS_TASK = http: /10.0.2.2:3002
